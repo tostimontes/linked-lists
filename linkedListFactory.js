168c61eab7a3d461a3a03842c92ff315fe03c341
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-02-09 09:34:25 +0100

    feat: first working version of linked lists
    - I will try to unify the traversal algorithm

--- linkedListFactory.js
+++ linkedListFactory.js
@@ -1,49 +1,111 @@
-// append(value) adds a new node containing value to the end of the list
-// prepend(value) adds a new node containing value to the start of the list
-// size returns the total number of nodes in the list
-// head returns the first node in the list
-// tail returns the last node in the list
-// at(index) returns the node at the given index
-// pop removes the last element from the list
-// contains(value) returns true if the passed in value is in the list and otherwise returns false.
-// find(value) returns the index of the node containing value, or null if not found.
-// toString represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
 const { createNewNode } = require('./nodeFactory');
 
 function createLinkedList() {
   const list = { head: null };
+  let total = 0;
   list.append = (value) => {
     const newNode = createNewNode(value);
     let obj = list.head;
     if (obj === null) {
       list.head = newNode;
+      return;
     }
     while (obj.next) {
       obj = obj.next;
     }
     obj.next = newNode;
+    total += 1;
   };
   list.prepend = (value) => {
     const newNode = createNewNode(value, list.head);
     list.head = newNode;
+    total += 1;
   };
+
   list.tail = () => {
-    while (Object.includes('next')) {}
+    let obj = list.head;
+    if (obj === null) {
+      return list;
+    }
+    while (obj.next) {
+      obj = obj.next;
+    }
+    return obj;
   };
   list.size = () => {
-    return Object.keys(list).length;
+    return total;
   };
-  list.at = (index) => {};
-  list.pop = () => {};
-  list.contains = (value) => {};
-  list.find = (value) => {};
-  list.toString = () => {};
+
+  list.at = (index) => {
+    let obj = list.head;
+    if (obj === null) {
+      return 'The list is empty';
+    }
+    for (let i = 0; i < index; i++) {
+      obj = obj.next;
+    }
+    return obj;
+  };
+  list.pop = () => {
+    let obj = list.head;
+    if (obj === null) {
+      return;
+    }
+    while (obj.next) {
+      if (obj.next.next === null) {
+        obj.next = null;
+        return;
+      }
+      obj = obj.next;
+    }
+  };
+  list.contains = (value) => {
+    let obj = list.head;
+    if (obj === null) {
+      return false;
+    }
+    while (obj.next) {
+      if (obj.value === value) {
+        return true;
+      }
+      obj = obj.next;
+    }
+    return false;
+  };
+  list.find = (value) => {
+    let obj = list.head;
+    let index = 0;
+    if (obj === null) {
+      return null;
+    }
+    while (obj.next) {
+      if (obj.value === value) {
+        return index;
+      }
+      index += 1;
+      obj = obj.next;
+    }
+    return null;
+  };
+  list.toString = () => {
+    let obj = list.head;
+    let ojbString = '';
+    if (obj === null) {
+      return 'The list is empty';
+    }
+    while (obj.next) {
+      ojbString += `(${obj.value}) -> `;
+      obj = obj.next;
+    }
+    ojbString += `(${obj.value}) -> null`;
+    return ojbString;
+  };
+  list.insertAt = (value, index) => {
+    const newNode = createNewNode(value, list.at(index));
+    list.at(index - 1).next = newNode;
+  };
+  list.removeAt = (index) => {
+    list.at(index - 1).next = list.at(index + 1);
+  };
   return list;
 }
-
-const linkedList = createLinkedList();
-
-linkedList.prepend('newaps');
-linkedList.size();
-linkedList.append('testappend');
-console.log(linkedList);
